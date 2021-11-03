7011c0ca9f3d8535eb886f49b26ddf27f6279744
bsef18a514 <91655339+bsef18a514@users.noreply.github.com>
2021-11-04 01:11:25 +0500

    intent message

--- MainActivity.java
+++ MainActivity.java
@@ -1,38 +1,24 @@
-package com.example.currencyconverter;
+package com.example.intentmessage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+    public class MainActivity extends AppCompatActivity {
+        public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
 
-
-    public void clickConvert(View v)
-    {
-        EditText unit = (EditText) findViewById(R.id.text1);
-        String temp = unit.getText().toString();
-
-        Double dollar = Double.parseDouble(temp);
-
-        Double pkr = dollar*174.35;
-
-
-        final TextView fresult;
-
-        fresult = (TextView) findViewById(R.id.result);
-        fresult.setText(String.valueOf(pkr));
-        fresult.setTextColor(Color.RED);
-
+        /** Called when the user taps the Send button */
+        public void sendMessage(View view) {
+            Intent intent = new Intent(this, DisplayMessageActivity.class);
+            EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+            String message = editText.getText().toString();
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
