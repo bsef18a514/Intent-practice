a3009a89c48f885eb5b7b8e68a8d42d3cb5eb704
bsef18a514 <91655339+bsef18a514@users.noreply.github.com>
2021-11-04 01:16:22 +0500

    sample

--- MainActivity.java
+++ MainActivity.java
@@ -1,24 +1,21 @@
-package com.example.intentmessage;
+package com.example.intentpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-    public class MainActivity extends AppCompatActivity {
-        public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-        }
 
-        /** Called when the user taps the Send button */
-        public void sendMessage(View view) {
-            Intent intent = new Intent(this, DisplayMessageActivity.class);
-            EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-            String message = editText.getText().toString();
-            intent.putExtra(EXTRA_MESSAGE, message);
-            startActivity(intent);
-        }
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
     }
-
+}
